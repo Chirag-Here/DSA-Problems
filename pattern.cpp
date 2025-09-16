7f896ada6f370c25c630282417afb2b126922ac7
CHirag-Here <chiragjoshi0204@gmail.com>
2025-09-16 13:07:22 +0530

    pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -12,4 +12,50 @@ int main(){
     }
     return 0;
 }
+Question 2 - Print a Triangle
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<i;                                                              (if u want to start every line from 1 use j)
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+Question 3 - Print Reverse Triangle 
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    for(int i=0; i<n; i++){                                         (if u dont use i=0 here its will start your output from 2nd line)
+        for(int j=i+1; j>0; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+Ouestion 4- Print Inverted Triangle                                 (Practice this one)
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){                                          
+        for(int j=0; j<i; j++){                                           (for space)
+            cout<<" ";
+        }
+    
+    for(int j=0; j<n-i; j++){                                             (for number)
+        cout<<(i+1);                                (add space here and it will become inverted piramid)
+    }
+    cout<<endl;
+    }
+    return 0;
+}
 */
